57d0ff9544d7746c953b5bc369312c062097db18
TOMAS FERNANDEZ ZABALA <47394082@ort>
2023-04-10 09:47:53 -0300

    Version 1.0

---
+++ Boxeador.cs
@@ -0,0 +1,28 @@
+class Boxeador {
+
+public string Nombre{get;set;}
+public string Pais{get;set;}
+public int Peso{get;set;}
+public int PotenciaGolpes{get; set;}
+public int VelocidadPiernas{get;set;}
+
+
+public Boxeador(string nombre, string pais, int peso, int pG, int vP){
+    Nombre = nombre;
+    Pais = pais;
+    Peso = peso;
+    PotenciaGolpes = pG;    
+    VelocidadPiernas = vP;
+}
+
+public double ObtenerSkill(){
+    double suma = 0;
+    suma = VelocidadPiernas*0.6;
+    suma += PotenciaGolpes * 0.8;
+    Random r = new Random();
+    int randomNum = r.Next(1,10);
+    suma += randomNum;
+    return suma;
+}
+
+}
